772f64df73d2307376fb28c7a0fc3d703a790bd2
Colin Swinney <colinjswinney@gmail.com>
2024-12-02 23:50:21 +0100

    update song archive

--- app/(tabs)/songs/index.tsx
+++ app/(tabs)/songs/index.tsx
@@ -1,11 +1,46 @@
-import { H2 } from "@expo/html-elements";
+import { Fragment } from "react";
+import { UL, LI } from "@expo/html-elements";
+import { Link } from "expo-router";
+import SONG_DATA from "@/data/songs.json";
+import Main from "@/components/HTML/Main";
 
-import Main from "../../../components/Main";
+type Song = {
+	slug: string;
+	title: string;
+};
 
-export default function ArtistsIndex() {
+export default function SongsIndex() {
+	const { songs } = SONG_DATA;
+
+	if (!songs) {
+		return null;
+	}
+
+	const renderItem = (song: Song) => {
+		return (
+			<LI>
+				<Link
+					key={song.slug}
+					href={{
+						pathname: "/songs/[slug]",
+						params: {
+							slug: song.slug,
+						},
+					}}
+				>
+					{song.title}
+				</Link>
+			</LI>
+		);
+	};
+
 	return (
 		<Main>
-			<H2>Songs</H2>
+			<UL>
+				{songs.map((song) => (
+					<Fragment key={song.slug}>{renderItem(song)}</Fragment>
+				))}
+			</UL>
 		</Main>
 	);
 }
