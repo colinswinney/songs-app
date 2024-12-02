e36e64cc39b8094000877981c9a582748ef093fa
Colin Swinney <colinjswinney@gmail.com>
2024-12-02 23:47:21 +0100

    update home as kitchen sink page for now

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,39 @@
-import { H2 } from "@expo/html-elements";
-
-import Main from "../../components/Main";
+import Main from "@/components/HTML/Main";
+import H1 from "@/components/HTML/H1";
+import H2 from "@/components/HTML/H2";
+import H3 from "@/components/HTML/H3";
+import H4 from "@/components/HTML/H4";
+import H5 from "@/components/HTML/H5";
+import H6 from "@/components/HTML/H6";
+import P from "@/components/HTML/P";
 
 export default function Index() {
 	return (
 		<Main>
-			<H2>Home</H2>
+			<H1>Lorem ipsum dolor sit amet</H1>
+			<H2>Lorem ipsum dolor sit amet</H2>
+			<H3>Lorem ipsum dolor sit amet</H3>
+			<H4>Lorem ipsum dolor sit amet</H4>
+			<H5>Lorem ipsum dolor sit amet</H5>
+			<H6>Lorem ipsum dolor sit amet</H6>
+			<P>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+				deserunt ex odio explicabo, veniam praesentium in quis cum nihil soluta?
+				Voluptatibus ex tempore molestiae iusto necessitatibus quaerat, quis
+				deleniti veritatis?
+			</P>
+			<P>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+				deserunt ex odio explicabo, veniam praesentium in quis cum nihil soluta?
+				Voluptatibus ex tempore molestiae iusto necessitatibus quaerat, quis
+				deleniti veritatis?
+			</P>
+			<P>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+				deserunt ex odio explicabo, veniam praesentium in quis cum nihil soluta?
+				Voluptatibus ex tempore molestiae iusto necessitatibus quaerat, quis
+				deleniti veritatis?
+			</P>
 		</Main>
 	);
 }
