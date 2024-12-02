acce0b0b3465447ea1d8acf1905a5c23a81aa3f8
Colin Swinney <colinjswinney@gmail.com>
2024-12-02 23:31:24 +0100

    create variable and global style constants

---
+++ constants/global-styles.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+import { COLOR, SIZE } from "@/constants/variables";
+
+const sharedHeadingStyles = {
+	fontWeight: 900 as 900,
+	marginBlockEnd: 16,
+	marginBlockStart: 0,
+	marginInline: 0,
+};
+
+export default StyleSheet.create({
+	h1: {
+		...sharedHeadingStyles,
+		fontSize: 48,
+	},
+	h2: {
+		...sharedHeadingStyles,
+		fontSize: 40,
+	},
+	h3: {
+		...sharedHeadingStyles,
+		fontSize: 33,
+	},
+	h4: {
+		...sharedHeadingStyles,
+		fontSize: 28,
+	},
+	h5: {
+		...sharedHeadingStyles,
+		fontSize: 23,
+	},
+	h6: {
+		...sharedHeadingStyles,
+		fontSize: 19,
+	},
+	main: {
+		padding: SIZE[16],
+	},
+	p: {
+		fontSize: 19,
+		fontWeight: "300",
+		marginBlock: 0,
+	},
+	safeAreaView: {
+		backgroundColor: COLOR.white,
+		flex: 1,
+	},
+});
---
+++ constants/variables.tsx
@@ -0,0 +1,19 @@
+export const COLOR = {
+	black: `#000`,
+	white: `#fcfbfa`,
+	primary: `rebeccapurple`,
+	primaryDark: `#3b246b`,
+	primaryLight: `#c6aaff`,
+	primaryLighter: `#e3d5ff`,
+};
+
+export const SIZE = {
+	8: 8,
+	12: 12,
+	16: 16,
+	24: 24,
+	32: 32,
+	48: 48,
+	64: 64,
+	96: 96,
+};
