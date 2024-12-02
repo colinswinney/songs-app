4d8bb46c9ff673fbdb4309e7273cc8912ad17f69
Colin Swinney <colinjswinney@gmail.com>
2024-12-02 23:51:03 +0100

    update song single

--- app/(tabs)/songs/[slug].tsx
+++ app/(tabs)/songs/[slug].tsx
@@ -1,11 +1,50 @@
-import { H2 } from "@expo/html-elements";
+import { BR, Span } from "@expo/html-elements";
+import { Tabs, useLocalSearchParams } from "expo-router";
+import SONG_DATA from "@/data/songs.json";
+import H2 from "@/components/HTML/H2";
+import H3 from "@/components/HTML/H3";
+import Main from "@/components/HTML/Main";
+import P from "@/components/HTML/P";
+import NotFound from "@/components/NotFound";
 
-import Main from "../../../components/Main";
+export default function SongsSlug() {
+	const { songs } = SONG_DATA;
+	const { slug } = useLocalSearchParams();
 
-export default function ArtistsSlug() {
+	const song = songs.find((song) => song.slug === slug);
+
+	if (!song) {
+		return <NotFound />;
+	}
+
 	return (
-		<Main>
-			<H2>Song single</H2>
-		</Main>
+		<>
+			<Tabs.Screen options={{ title: song.title }} />
+			<Main>
+				<H2>{song.title}</H2>
+				{song.sections.map((section, index) => (
+					<>
+						<H3 key={index}>
+							{section.name}
+						</H3>
+						{section.lyrics.map((lyric, index) => (
+							<P key={index}>
+								{lyric}
+							</P>
+						))}
+						<BR />
+						{section.chords.map((line, index) => (
+							<P key={index}>
+								{line.map((chord, index) => (
+									<Span key={index}>
+										{chord.note}
+									</Span>
+								))}
+							</P>
+						))}
+					</>
+				))}
+			</Main>
+		</>
 	);
 }
